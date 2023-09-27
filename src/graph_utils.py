b0718d0006f9c1f7edd24724ea02b6aff9b442f9
Igor Gaidai <gaidai92@gmail.com>
2023-09-27 15:51:21 -0400

    Adds isomorphism checks and edge bfs

--- src/graph_utils.py
+++ src/graph_utils.py
@@ -1,10 +1,50 @@
 """
 Graph utilities.
 """
+from queue import SimpleQueue
+from typing import Iterable, Collection, Sequence
+
 import networkx as nx
 import numpy as np
 from networkx import Graph
 from numpy import ndarray
+
+
+def edge_bfs(graph: Graph, starting_edge: tuple) -> dict[tuple, int]:
+    """
+    Carries out edge BFS from the specified edge and returns distances to all other edges.
+    :param graph: Graph for BFS.
+    :param starting_edge: Starting edge.
+    :return: Distances to all edges starting from the given edge.
+    """
+    if starting_edge[0] > starting_edge[1]:
+        starting_edge = starting_edge[::-1]
+    distances = {starting_edge: 0}
+    bfs_queue = SimpleQueue()
+    bfs_queue.put(starting_edge)
+    while not bfs_queue.empty():
+        parent_edge = bfs_queue.get()
+        for node in parent_edge:
+            for edge in graph.edges(node):
+                if edge[0] > edge[1]:
+                    edge = edge[::-1]
+                if edge not in distances:
+                    distances[edge] = distances[parent_edge] + 1
+                    bfs_queue.put(edge)
+    return distances
+
+
+def get_max_edge_depth(graph: Graph) -> int:
+    """
+    Returns worst case depth of edge BFS.
+    :param graph: Graph in question.
+    :return: Worst case depth of edge BFS.
+    """
+    depths = []
+    for edge in graph.edges:
+        distances = edge_bfs(graph, edge)
+        depths.append(max(distances.values()))
+    return max(depths)
 
 
 def get_edge_diameter(graph: Graph) -> int:
@@ -49,7 +89,41 @@ def get_index_edge_list(graph: Graph, edge_list: list[tuple[int, int]] = None) -
 
 
 def read_graph_xqaoa(path):
+    """
+    Reads a graph in XQAOA format
+    :param path: Path to graph file.
+    :return: Read graph.
+    """
     with open(path) as f:
         lines = f.readlines()
     lines = lines[2:]
     return nx.read_edgelist(lines, delimiter=',')
+
+
+def is_isomorphic(graph: Graph, other_graphs: Sequence) -> bool:
+    """
+    Checks if given graph is isomorphic to any of the other graphs.
+    :param graph: Graph to check.
+    :param other_graphs: Graphs to check against.
+    :return: True if the graph is isomorphic to any of the graphs, False otherwise.
+    """
+    for i in range(len(other_graphs)):
+        if nx.is_isomorphic(graph, other_graphs[i]):
+            return True
+    return False
+
+
+def find_non_isomorphic(graphs: Sequence) -> list[bool]:
+    """
+    Finds non-isomorphic graphs among the given iterable.
+    :param graphs: Graphs to search.
+    :return: Boolean list such that if True elements are taken from graphs, then none of them will be isomorphic.
+    """
+    res = [True] * len(graphs)
+    for pivot in range(len(graphs)):
+        if not res[pivot]:
+            continue
+        for i in range(pivot + 1, len(graphs)):
+            if nx.is_isomorphic(graphs[pivot], graphs[i]):
+                res[i] = False
+    return res
