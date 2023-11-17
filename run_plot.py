Changes WorkerConstant's constant to 0.2, fixes average nfev print after calculation

run_plot.py
@@ -42,12 +42,12 @@ def plot_qaoa_expectation_p1(graph: Graph, edge_list: list[tuple[int, int]] = No
     return surf
 
 
-def plot_ar_vs_p_heuristics_core(search_space: str, methods: list[str], labels: list[str], attempts: str, min_y: float, max_p: int):
+def plot_ar_vs_p_heuristics_core(methods: list[str], labels: list[str], min_y: float, max_p: int):
     lines = []
     for method_ind, method in enumerate(methods):
-        ps, p_series = get_column_statistic(f'graphs/new/nodes_9/depth_3/output/{search_space}/{method}/attempts_{attempts}/out.csv', r'p_\d+$', np.mean)
+        ps, p_series = get_column_statistic(f'graphs/new/nodes_9/depth_3/output/{method}/out.csv', r'p_\d+$', np.mean)
         lines.append(Line(ps[:max_p], p_series[:max_p], colors[method_ind], style='-', label=labels[method_ind]))
-        ps, p_series = get_column_statistic(f'graphs/new/nodes_9/depth_3/output/{search_space}/{method}/attempts_{attempts}/out.csv', r'p_\d+$', min)
+        ps, p_series = get_column_statistic(f'graphs/new/nodes_9/depth_3/output/{method}/out.csv', r'p_\d+$', min)
         lines.append(Line(ps[:max_p], p_series[:max_p], colors[method_ind], style='--'))
 
     x_lim = [0.75, max_p + 0.25]
@@ -58,9 +58,10 @@ def plot_ar_vs_p_heuristics_core(search_space: str, methods: list[str], labels: 
 
 
 def plot_ar_vs_p_heuristics_qaoa_attempts_1():
-    methods = ['constant', 'tqa', 'interp', 'fourier', 'random']
-    labels = ['Constant', 'TQA', 'Interp', 'Fourier', 'Random']
-    plot_ar_vs_p_heuristics_core('qaoa', methods, labels, '1', 0.52, 10)
+    methods = ['constant/0.01', 'tqa/attempts_1', 'interp/attempts_1', 'fourier/attempts_1', 'random/attempts_1', 'constant/0.1/L-BFGS-B']
+    methods = ['qaoa/' + method for method in methods]
+    labels = ['Constant 0.01', 'TQA', 'Interp', 'Fourier', 'Random', 'Constant 0.1']
+    plot_ar_vs_p_heuristics_core(methods, labels, 0.52, 10)
     save_figure()
     plt.show()
 
@@ -560,10 +561,10 @@ def plot_converged_fraction_vs_rel_p_r10_edges():
 
 
 if __name__ == "__main__":
-    # plot_ar_vs_p_heuristics_qaoa_attempts_1()
+    plot_ar_vs_p_heuristics_qaoa_attempts_1()
     # plot_ar_vs_p_heuristics_qaoa_attempts_p()
     # plot_ar_vs_p_heuristics_ma_attempts_1()
-    plot_ar_vs_p_nodes()
+    # plot_ar_vs_p_nodes()
     # plot_ar_vs_p_depths()
 
     # plot_avg_ar_vs_p_interp_nodes()

src/parallel.py
@@ -169,7 +169,8 @@ class WorkerStandard(WorkerBaseQAOA):
 
         graph = self.reader(path)
         evaluator = Evaluator.get_evaluator_standard_maxcut(graph, self.p, search_space=search_space)
-        options = {'maxiter': np.iinfo(np.int32).max}
+        maxint = np.iinfo(np.int32).max
+        options = {'maxiter': maxint, 'maxfun': maxint}
 
         try:
             result = optimize_qaoa_angles(evaluator, options=options, **optimize_args)
@@ -196,8 +197,8 @@ class WorkerConstant(WorkerStandard):
         self.search_space = 'qaoa'
 
     def provide_guess(self, **kwargs):
-        gammas = [0.1] * self.p
-        betas = [-0.1] * self.p
+        gammas = [0.2] * self.p
+        betas = [-0.2] * self.p
         starting_angles = np.array(list(it.chain(*zip(gammas, betas))))
         return starting_angles
 
@@ -537,4 +538,4 @@ def optimize_expectation_parallel(dataframe_path: str, rows_func: callable, num_
 
     dataset_id = re.search(r'nodes_\d+/depth_\d+', dataframe_path)[0]
     print(f'dataset: {dataset_id}; p: {worker.p}; mean: {np.mean(df[worker.out_col]):.3f}; min: {min(df[worker.out_col]):.3f}; converged: {sum(df[worker.out_col] > 0.9995)}; '
-          f'nfev: {np.mean(df[worker.out_col + "_nfev"]):.0f}\n')
+          f'nfev: {np.mean(df[worker.out_col + "_nfev"].where(lambda x: x != 0)):.0f}\n')