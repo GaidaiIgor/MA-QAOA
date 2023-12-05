88bb0b43c2b9d63d8471c009d0875755298feb21
Igor Gaidai <gaidai92@gmail.com>
2023-12-05 01:15:06 -0500

    Adds basic usage examples

--- README.md
+++ README.md
@@ -4,10 +4,11 @@ Implementation of the original Quantum Approximate Optimization Algorithm (QAOA)
 Both algorithms are designed to find approximate solutions to combinatorial optimization problems on quantum computers. 
 
 This implementation is purely classical, so it only calculates expectation values and does not give explicit answers to the combinatorial problems (since this usually requires a quantum computer).
-It works on arbitrary weighted graphs, including graphs with triangles.
 
-To run the code, run `run_qaoa.py`. Run parameters and input graph can be specified in the code.
+See basic usage examples in `basic_examples.py`
 
 For more info on QAOA refer to https://arxiv.org/abs/1411.4028v1
 
 For more info on MA-QAOA refer to https://www.nature.com/articles/s41598-022-10555-8
+
+See also https://arxiv.org/abs/2312.00200 for the results calculated with this repo.
---
+++ basic_examples.py
@@ -0,0 +1,37 @@
+"""
+Examples of basic uses.
+"""
+import networkx as nx
+import numpy as np
+
+from src.optimization import Evaluator, optimize_qaoa_angles
+
+
+def run_point():
+    """ Evaluates cost expectation at the given QAOA angles. """
+    graph = nx.read_gml('graphs/new/nodes_9/depth_3/0.gml', destringizer=int)
+    p = 2
+    # Could also be set to 'ma' for MA-QAOA. Other options are available as well, see Evaluator class for more details.
+    search_space = 'qaoa'
+    evaluator = Evaluator.get_evaluator_standard_maxcut(graph, p, search_space=search_space)
+    # Angles are given in the order of application, i.e. gamma_1, beta_1, gamma_2, beta_2, etc.
+    # In case of MA-QAOA the order of gammas and betas matches the order of graph.edges and graph.nodes
+    angles = np.array([0.1] * evaluator.num_angles) * np.pi
+    cut_expectation = -evaluator.func(angles)
+    print(f'Expectation: {cut_expectation}')
+
+
+def run_optimization():
+    """ Optimizes cost expectation. """
+    graph = nx.read_gml('graphs/new/nodes_9/depth_3/0.gml', destringizer=int)
+    p = 2
+    search_space = 'qaoa'
+    evaluator = Evaluator.get_evaluator_standard_maxcut(graph, p, search_space=search_space)
+    # Picks random starting angles if None are given
+    result = optimize_qaoa_angles(evaluator, starting_angles=None, method='L-BFGS-B')
+    print(f'Best achieved objective: {-result.fun}')
+    print(f'Maximizing angles: {repr(result.x / np.pi)}')
+
+
+if __name__ == '__main__':
+    run_optimization()
